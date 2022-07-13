1f84575c987922894a8c62676365ea5b4751e479
Simon Cozens <simon@simon-cozens.org>
2022-07-13 22:53:21 +0100

    Move __main__ to main for poetrification

--- Lib/fontninja/__main__.py
+++ Lib/fontninja/__main__.py
@@ -3,10 +3,14 @@ import fontninja.build
 import fontninja.plumbing
 import sys
 
-parser = argparse.ArgumentParser(description='Font builder')
+def main():
+	parser = argparse.ArgumentParser(description='Font builder')
 
-if len(sys.argv) > 0 and sys.argv[1] == "--plumbing":
-	del sys.argv[1]
-	fontninja.plumbing.run(parser)
-else:
-	fontninja.build.run(parser)
+	if len(sys.argv) > 1 and sys.argv[1] == "--plumbing":
+		del sys.argv[1]
+		fontninja.plumbing.run(parser)
+	else:
+		fontninja.build.run(parser)
+
+if __name__ == "__main__":
+	main()
